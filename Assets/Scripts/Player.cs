cc6bfe32d49592d99e6bda5483d1778010c006a1
Sebastian <s.hartfeil@web.de>
2020-06-10 13:46:23 +0200

    fix snap axes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,7 @@ public class Player : NetworkBehaviour
     // Use this for initialization
     void Start () {
         if (isLocalPlayer) {
-            GameObject cam = Instantiate(camPrefab, new Vector3(transform.position.x, transform.position.), Quaternion.identity);
+            GameObject cam = Instantiate(camPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), Quaternion.identity);
             cam.transform.parent = transform;
         }
 	}
